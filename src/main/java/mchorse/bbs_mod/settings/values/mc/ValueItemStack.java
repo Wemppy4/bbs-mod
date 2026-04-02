d340f9fb6ddeeea38e1294d9efbcd4f8dfc1bd57
wemppyy <denisfrostaliuk@gmail.com>
2026-04-02 23:54:48 +0200

    refactor(mc): update ValueItemStack to extend BaseKeyframeFactoryValue
    - Changed ValueItemStack to extend BaseKeyframeFactoryValue, enhancing its functionality with keyframe support.
    - Updated constructor to utilize KeyframeFactories for improved item stack handling.

--- src/main/java/mchorse/bbs_mod/settings/values/mc/ValueItemStack.java
+++ src/main/java/mchorse/bbs_mod/settings/values/mc/ValueItemStack.java
@@ -1,26 +1,13 @@
 package mchorse.bbs_mod.settings.values.mc;
 
-import mchorse.bbs_mod.data.types.BaseType;
-import mchorse.bbs_mod.settings.values.base.BaseValueBasic;
+import mchorse.bbs_mod.settings.values.base.BaseKeyframeFactoryValue;
 import mchorse.bbs_mod.utils.keyframes.factories.KeyframeFactories;
 import net.minecraft.item.ItemStack;
 
-public class ValueItemStack extends BaseValueBasic<ItemStack>
+public class ValueItemStack extends BaseKeyframeFactoryValue<ItemStack>
 {
     public ValueItemStack(String id)
     {
-        super(id, ItemStack.EMPTY);
-    }
-
-    @Override
-    public BaseType toData()
-    {
-        return KeyframeFactories.ITEM_STACK.toData(this.value);
-    }
-
-    @Override
-    public void fromData(BaseType data)
-    {
-        this.set(KeyframeFactories.ITEM_STACK.fromData(data));
+        super(id, KeyframeFactories.ITEM_STACK, ItemStack.EMPTY);
     }
 }
